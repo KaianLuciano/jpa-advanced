951e84498de041fd2e5d7933bf26eee8ebe04330
KaianLuciano <kaianlucianoalvesdossantos@gmail.com>
2024-07-14 09:24:44 -0300

    Create Cliente

---
+++ src/main/java/br/com/alura/loja/modelo/Pedido.java
@@ -0,0 +1,73 @@
+package br.com.alura.loja.modelo;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+@Table(name = "pedidos")
+public class Pedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal valorTotal;
+    private LocalDate data = LocalDate.now();
+    @ManyToOne
+    private Cliente cliente;
+
+    public Pedido() {
+    }
+
+    public Pedido(Long id, BigDecimal valorTotal, LocalDate data, Cliente cliente) {
+        this.id = id;
+        this.valorTotal = valorTotal;
+        this.data = data;
+        this.cliente = cliente;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Pedido)) return false;
+        Pedido pedido = (Pedido) o;
+        return Objects.equals(getId(), pedido.getId()) && Objects.equals(getValorTotal(), pedido.getValorTotal()) && Objects.equals(getData(), pedido.getData()) && Objects.equals(getCliente(), pedido.getCliente());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getValorTotal(), getData(), getCliente());
+    }
+}
